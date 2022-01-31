e070c762f37a1a68e9fa17fc47d6141a2d1ed26f
Will Tesler <willtesler@gmail.com>
2022-01-30 20:19:05 -0500

    Only wrap if not already wrapped.

--- src/Wrapper/RootOverlays.js
+++ src/Wrapper/RootOverlays.js
@@ -2,22 +2,26 @@ import React from 'react';
 import Toast from "../Toast/Toast";
 import Tooltip from "../Tooltip/Tooltip";
 import FullscreenDialog from "../Dialog/FullscreenDialog";
+import {withModule} from "react-hoc-di";
 
 /**
  * Wraps children in all the useful root overlays.
  *
  * Ensures root overlays are wrapped in proper order.
  */
-const RootOverlays = ({children}) => {
-  return (
-    <Toast>
-      <Tooltip>
-        <FullscreenDialog>
-          {children}
-        </FullscreenDialog>
-      </Tooltip>
-    </Toast>
-  );
+const RootOverlays = ({children, module}) => {
+  const {toastRelay, tooltipRelay, dialogRelay} = module;
+  let element = children;
+  if (!dialogRelay) {
+    element = (<FullscreenDialog> {children} </FullscreenDialog>);
+  }
+  if (!tooltipRelay) {
+    element = (<Tooltip> {children} </Tooltip>);
+  }
+  if (!toastRelay) {
+    element = (<Toast> {children} </Toast>);
+  }
+  return element;
 }
 
-export default RootOverlays;
+export default withModule(RootOverlays);
